6cff252070068f35cfce69cecce1e735c822b922
adamkali <adamkali@outlook.com>
2021-02-26 08:55:21 -0500

    I dont know if this is going to work but lets try...

--- src/bin/errs/LogInException.java
+++
@@ -1,8 +0,0 @@
-package CardGame.src.bin.errs;
-
-public class LogInException extends RuntimeException {
-
-    private static final long serialVersionUID = 1056849320L;
-
-    public LogInException(String message) { super(message); }
-}
